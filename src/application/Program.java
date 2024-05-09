6a987b70ae3785c6e7bb5d6bc09ccff7dfbc5fc0
Ericsson Luis <ericssonluis83@gmail.com>
2024-05-09 19:01:40 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,14 +7,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	 public static void main(String[] args) throws SQLException  {
     
 		Department obj = new Department(1,"books");
-       System.out.print(obj);
+		Seller d = new Seller(21,"bob","bob@gmail.com",new Date(),3000.0,obj);
+       System.out.print(d);
 	}
 
 }
